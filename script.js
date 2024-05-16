750e21fa3400068b628c5603fe3356f35e60bbcd
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-16 11:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,40 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function download(img) {
+  let promise = new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = img.url;
+    console.log("new Image in promise", image);
+    image.onload = () => resolve(image);
+    image.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+  });
+  console.log(promise);
+  return promise;
+}
+
+function display(loadedImg) {
+  output.innerHTML = "";
+  loadedImg.forEach((img) => {
+    output.append(img);
+  });
+}
+
+function start() {
+  let promises = images.map((img) => {
+    return download(img);
+  });
+  Promise.all(promises)
+    .then((loadedImg) => {
+      console.log(".then loadedimg", loadedImg);
+      display(loadedImg);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+btn.addEventListener("click", () => {
+  start();
+});
